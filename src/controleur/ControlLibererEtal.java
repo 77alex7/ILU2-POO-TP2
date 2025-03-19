a6cf70e8c5cea7e4c22b15ac6335b963bec9b902
77alex7 <alexis.bruniera@univ-tlse3.fr>
2025-03-19 12:03:31 +0100

    TP2&3 -8

--- src/controleur/ControlLibererEtal.java
+++ src/controleur/ControlLibererEtal.java
@@ -1,29 +1,36 @@
 package controleur;
 
+import villagegaulois.Etal;
+
 public class ControlLibererEtal {
 	private ControlTrouverEtalVendeur controlTrouverEtalVendeur;
 
-	public ControlLibererEtal(
-			ControlTrouverEtalVendeur controlTrouverEtalVendeur) {
+	public ControlLibererEtal(ControlTrouverEtalVendeur controlTrouverEtalVendeur) {
 		this.controlTrouverEtalVendeur = controlTrouverEtalVendeur;
 	}
 
-	//TODO a completer
+	public boolean isVendeur(String nomVendeur) {
+		Etal etal=controlTrouverEtalVendeur.trouverEtalVendeur(nomVendeur);
+		boolean vendeurReconnu=etal!=null;
+		return vendeurReconnu;
+	}
 
 	/**
 	 * 
 	 * @param nomVendeur
-	 * @return donneesEtal est un tableau de chaine contenant
-	 * 		[0] : un boolean indiquant si l'étal est occupé
-	 * 		[1] : nom du vendeur
-	 * 		[2] : produit vendu
-	 * 		[3] : quantité de produit à vendre au début du marché
-	 * 		[4] : quantité de produit vendu
+	 * @return donneesEtal est un tableau de chaine contenant [0] : un boolean
+	 *         indiquant si l'étal est occupé [1] : nom du vendeur [2] : produit
+	 *         vendu [3] : quantité de produit à vendre au début du marché [4] :
+	 *         quantité de produit vendu
 	 */
 	public String[] libererEtal(String nomVendeur) {
-		//TODO a completer
 		String[] donneesEtal = null;
-		return donneesEtal;
+		Etal etal=controlTrouverEtalVendeur.trouverEtalVendeur(nomVendeur);
+		if (etal!=null) {
+			donneesEtal = etal.etatEtal();
+			return donneesEtal;
+		}
+		return null;
 	}
 
 }
--- src/frontiere/BoundaryAcheterProduit.java
+++ src/frontiere/BoundaryAcheterProduit.java
@@ -10,6 +10,29 @@ public class BoundaryAcheterProduit {
 	}
 
 	public void acheterProduit(String nomAcheteur) {
-		// TODO Ã  completer
+		if (nomAcheteur existe pas) {
+			System.out.println("Je suis désolée"nomAcheteur"mais il faut être un habitant de notre village pour commercer ici.");
+		}else {
+			String produit=Clavier.entrerChaine("Quelle produit voulez vous acheter?");
+			//String produitExist= trouver produit;
+			if (produitExist==null) {
+				System.out.println("Désolé, personne ne vend ce produit au marché");
+			}
+			else {
+				System.out.println(nomAcheteur+" se déplace jusqu'à l'étal du vendeur "+vendeur);
+				System.out.println("Bonjour"+nomAcheteur);
+				int quantite=Clavier.entrerEntier("Combien de fleur voulez-vous acheter?");
+				if (quantité ok) {
+					System.out.println("Panoramix achète " +quantite+produit+" à "+nomVendeur);
+					//-5fleurs au vendeur
+				}else {
+					if(quantité >0 mais quantite>nbFleur){
+					System.out.println(nomAcheteur" veut acheter "+quantite+ produit+", malheureusement"+nomVendeur+" n'en a plus que 3."+nomAcheteur+" achète tout le stock de "+nomVendeur);
+					}else {
+						System.out.println(nomAcheteur"achète "+quantite+produit+", malheureusement il n'y en a plus !");
+					}
+				}
+			}
+		}	
 	}
 }
