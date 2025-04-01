51704174c3eaa6760f849ddf515788cb8e92d399
Alexis Bruniera <alexis.bruniera@univ-tlse3.fr>
2025-04-01 13:40:40 +0200

    presque fini

--- src/controleur/ControlAcheterProduit.java
+++ src/controleur/ControlAcheterProduit.java
@@ -30,12 +30,11 @@ public class ControlAcheterProduit {
 		int quantite=etal.getQuantite();
 		return quantite;
 	}
-	public int acheterProduit(String vendeur, int quantiteVendu) {
+	public int acheterProduit(String vendeur, int quantiteAAcheter) {
 		Gaulois sVendeur=village.trouverHabitant(vendeur);
 		Etal etal=village.rechercherEtal(sVendeur);
-//		int quantite=etal.getQuantite();
-//		quantite=quantite-quantiteVendu;
-		int quantiteAcheter=etal.acheterProduit(quantiteVendu);
+		int quantiteAcheter=etal.acheterProduit(quantiteAAcheter);
 		return quantiteAcheter;
+		//dans etal.acheterProduit on a pas le cas ou la quantité a acheter = quantité présente et on a pas la cas ou la quantité est supp a la quantite
 	}
 }
